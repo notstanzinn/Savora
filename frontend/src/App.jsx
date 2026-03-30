79fa3d7580e059a40836c213c88a357ff12369c7
notstanzinn <codeforces0608@gmail.com>
2026-03-30 23:53:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ import Shop from './pages/Shop'
 import { useEffect } from 'react'
 import {io} from "socket.io-client"
 import { setSocket } from './redux/userSlice'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://savora-backend-qhu7.onrender.com"
 function App() {
   const dispatch = useDispatch()
   const {userData} = useSelector(state=>state.user)
